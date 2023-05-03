901685d601fa10b16b10b734756322019b28f735
Helen Lehrer <hlehrer@outlook.com>
2023-05-03 14:57:43 -0700

    Add register prototype frame to readme

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# _heARTconnection_
+### _A therapeautic arts social media web app_
+
+##
+### Prototype WIP
+<p align="center">
+<img src=./readme-assets/Register.png width="70%">
+</p>
--- backend/server.ts
+++ backend/server.ts
@@ -1,8 +1,16 @@
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import multer from 'multer';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+/*MIDDLEWARE*/
 
 const app = express();
-
+app.use(express.json());
 app.use(
   cors({
     origin: [
