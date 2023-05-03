77718b28ef4d78a7889457873a9acf7905917c81
Helen Lehrer <hlehrer@outlook.com>
2023-05-03 11:00:49 -0700

    Add API fetch & parse TS exercise

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,40 @@
 import Image from 'next/image'
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
+import type { GetStaticProps, NextPage } from "next";
+import { Character, GetCharacterResults } from "../../types";
 
-export default function Home() {
+const Home: NextPage<{characters: Character[]}> = ({characters}) => {
   return (
-    <Head>
-      <title>heARTconnection</title>
-    </Head>
-  )
-}
+    <div>
+      <Head>
+        <title>heARTconnection</title>
+      </Head>
+      {characters.map((character) => {
+        return <div key={character.id}>{character.name}
+        <Image
+          src={character.image}
+          alt={character.name}
+          width="200"
+          height="200"
+        />
+        </div>
+      })}
+    </div>
+  );
+};
+
+//this will run at build time
+export const getStaticProps: GetStaticProps = async (context) => {
+const res = await fetch("https://rickandmortyapi.com/api/character")
+const { results }: GetCharacterResults = await res.json();
+
+return {
+  props: {
+    characters:results,
+    },
+  };
+};
+
+//this will run at runtime
+export default Home; 
