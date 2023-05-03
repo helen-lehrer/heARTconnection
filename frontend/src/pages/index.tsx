eb0f4e30477c5ca1882ed07ca93206cbcb1535da
Helen Lehrer <hlehrer@outlook.com>
2023-05-03 10:23:50 -0700

    Add jest testing config

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import Head from 'next/head'
+import { Inter } from 'next/font/google'
+
+export default function Home() {
+  return (
+    <Head>
+      <title>heARTconnection</title>
+    </Head>
+  )
+}
