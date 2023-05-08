72ddee58fdc1945d4d47685659f88127df398f0a
Helen Lehrer <hlehrer@outlook.com>
2023-05-08 16:11:46 -0700

    redo create-t3-app without nextauth

--- src/pages/api/auth/[...nextauth].ts
+++
@@ -1,4 +0,0 @@
-import NextAuth from "next-auth";
-import { authOptions } from "@/components/server/auth";
-
-export default NextAuth(authOptions);
--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,8 +1,8 @@
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 
-import { env } from "@/components/env.mjs";
-import { createTRPCContext } from "@/components/server/api/trpc";
-import { appRouter } from "@/components/server/api/root";
+import { env } from "~/env.mjs";
+import { createTRPCContext } from "~/server/api/trpc";
+import { appRouter } from "~/server/api/root";
 
 // export API handler
 export default createNextApiHandler({
